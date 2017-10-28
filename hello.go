6894d925ec252fb0e79513b18b7f85e58feb9aef
gasegi <1353377@gmail.com>
2017-10-29 00:15:03 +0900

    (p36) Use of defer

--- hello.go
+++ hello.go
@@ -2,37 +2,27 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"log"
-	"net/http"
 	"os"
-	"path"
-	"path/filepath"
 )
 
 func main() {
-	cwd, err := os.Getwd()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// http リクエストは論理パスなので path パッケージを使う
-		if ok, err := path.Match("/data/*.txt", r.URL.Path); err != nil || !ok {
-			http.NotFound(w, r)
-			return
-		}
-
-		// 以降はパスを物理パスとして扱うので、 path/filepath パッケージを使う
-		name := filepath.Join(cwd, "data", filepath.Base(r.URL.Path))
-		f, err := os.Open(name)
-		if err != nil {
-			http.NotFound(w, r)
-			return
-		}
-		defer f.Close()
-		io.Copy(w, f)
-	})
-	http.ListenAndServe(":8080", nil)
+	doSomething()
 	fmt.Printf("Hello, world.\n")
+}
+
+func doSomething() error {
+	err := os.MkdirAll("newdir", 0755)
+	if err != nil {
+		return err
+	}
+	// (2) 次にディレクトリが削除される
+	defer os.RemoveAll("newdir")
+
+	f, err := os.Create("newdir/newfile")
+	if err != nil {
+		return err
+	}
+	// (1) 最初にファイルハンドルが閉じられる
+	defer f.Close()
+	return nil
 }
