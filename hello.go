20fdd4220c45d0d724c048b2761d90585085464d
gasegi <1353377@gmail.com>
2017-10-28 23:02:51 +0900

    (p34) Use of path and path/filepath properly

--- hello.go
+++ hello.go
@@ -2,22 +2,37 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"os"
-	"os/user"
+	"path"
 	"path/filepath"
 )
 
 func main() {
-	u, err := user.Current()
+	cwd, err := os.Getwd()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	dir := filepath.Join(u.HomeDir, ".config", "myapp")
-	err = os.MkdirAll(dir, 0755)
-	if err != nil {
-		log.Fatal(err)
-	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// http リクエストは論理パスなので path パッケージを使う
+		if ok, err := path.Match("/data/*.txt", r.URL.Path); err != nil || !ok {
+			http.NotFound(w, r)
+			return
+		}
+
+		// 以降はパスを物理パスとして扱うので、 path/filepath パッケージを使う
+		name := filepath.Join(cwd, "data", filepath.Base(r.URL.Path))
+		f, err := os.Open(name)
+		if err != nil {
+			http.NotFound(w, r)
+			return
+		}
+		defer f.Close()
+		io.Copy(w, f)
+	})
+	http.ListenAndServe(":8080", nil)
 	fmt.Printf("Hello, world.\n")
 }
