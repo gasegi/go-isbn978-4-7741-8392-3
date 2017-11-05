013fb5fdf37f89a4277fd74fdf83fc499f2b6d7b
gasegi <1353377@gmail.com>
2017-11-05 18:32:25 +0900

    [WIP] (p47) Use go-bindata
    go-bindata が使用できないため、後回し

--- hello.go
+++ hello.go
@@ -1,27 +1,43 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
-	"log"
-	"os/exec"
-	"runtime"
+	"io"
+	"mime"
+	"net/http"
+	"path"
+	"strconv"
 )
 
-func main() {
-	doSomething()
-	fmt.Printf("Hello, world.\n")
+// 以下のコメントが go generate で使用される
+
+// go:generate go-bindata -prefix data ./data
+
+func staticHandler(w http.ResponseWriter, req *http.Request) {
+	p := req.URL.Path
+	if p == "" {
+		p = "index.html"
+	}
+
+	// リクエストパスから静的コンテンツを得る
+	bs, err := Asset(p)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	// 拡張子から Content-Type を調べる
+	if ctype := mime.TypeByExtension(path.Ext(p)); ctype != "" {
+		w.Header().Set("Content-Type", ctype)
+
+	}
+	w.Header().Set("Content-Length", strconv.Itoa(len(bs)))
+	io.Copy(w, bytes.NewBuffer(bs))
 }
 
-func doSomething() {
-	var cmd *exec.Cmd
-	if runtime.GOOS == "windows" {
-		cmd = exec.Command("cmd", "/c", "ipconfig")
-	} else {
-		cmd = exec.Command("/bin/sh", "-c", "ip addr")
-	}
-	output, err := cmd.Output()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(output)
+func main() {
+	http.Handle("/", http.StripPrefix("/", http.HandlerFunc(staticHandler)))
+	http.ListenAndServe(":3000", nil)
+	fmt.Printf("Hello, world.\n")
 }
